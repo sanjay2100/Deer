38af98126ec1858958d0e611f5b8a82aaeb44e13
sanjay.manikandan <sanjay.manikandan@fiaglobal.com>
2024-11-16 16:01:19 +0530

    modified ui

--- src/Components/Drawer.tsx
+++ src/Components/Drawer.tsx
@@ -1,4 +1,4 @@
-import { Grid, Stack, Typography } from "@mui/material";
+import { Avatar, Grid, Stack, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { SetStateAction } from "react";
 import DataTable from "./sub_components/All_Products";
@@ -17,31 +17,39 @@ interface AppProps{
 }
 
 const AppDrawer:React.FC<AppProps>=(Props:AppProps)=>{
+
   return(
-    <Grid container sx={{height:'100vh'}}>
-      <Grid item xl={3} lg={2} sx={{backgroundColor:"#fff",height:'100%',borderRight:'0.5px solid #d6d1c5'}}>
+    <Grid container sx={{height:'100vh',backgroundColor:'whitesmoke'}}>
+      <Grid item xl={3} lg={2} sx={{backgroundColor:"whitesmoke",height:'100%'}}>
           <Stack>
-            <Box sx={{padding:'15px',display:'flex',justifyContent:'flex-start',alignItems:'center'}}>
+            <Box sx={{padding:'20px',display:'flex',justifyContent:'flex-start',alignItems:'center'}}>
               <Typography sx={{fontWeight:650,fontSize:'21px'}}>EZCart</Typography>
             </Box>
-            <Stack sx={{paddingInline:'8px'}}>
+            <Stack sx={{paddingInline:'8px'}} gap={1}>
               {Props.menu.map((item,index)=>(
                 <Box
                   key={index}
                   onClick={()=>Props.setSelected(item.menu)}
-                  sx={{padding:'15px',cursor:'pointer',backgroundColor:`${Props.selected===item.menu?"#efe9f7":"#fff"}`,borderRadius:'8px'}}
+                  sx={{padding:'10px',cursor:'pointer',backgroundColor:`${Props.selected===item.menu?"#fff":"whitesmoke"}`,color:Props.selected===item.menu?'#000':"#000",borderRadius:'8px',border:Props.selected===item.menu?'0.5px solid #edebe4':"none"}}
                 >
                   <Stack direction="row" gap={1} alignItems="center">
                   <Box>{item.icon}</Box>
-                  <Typography sx={{fontWeight:500,fontSize:'18px'}}>{item.menu}</Typography>
+                  <Typography sx={{fontWeight:500,fontSize:'14px'}}>{item.menu}</Typography>
                   </Stack>
                 </Box>
               ))}
             </Stack>
           </Stack>
       </Grid>
-      <Grid item xl={9} md={9} sx={{padding:'15px',height:'100vh'}}>
-        <Stack  justifyContent="center" alignItems="center">
+      <Grid item xl={9} md={10} sx={{height:'100vh',backgroundColor:'#fff'}}>
+        <Grid container height="10%" sx={{backgroundColor:'whitesmoke'}}>
+          <Grid item md={12} xs={12}>
+            <Stack sx={{paddingInline:'15px',paddingTop:'15px',paddingBottom:'15px',alignItems:'flex-end'}}>
+              <Avatar>N</Avatar>
+            </Stack>
+          </Grid>
+        </Grid>
+        <Stack sx={{padding:'15px'}} justifyContent="center" alignItems="center" mt={1}>
               {
                 Props.selected==="My products"?
                   <DataTable/>
